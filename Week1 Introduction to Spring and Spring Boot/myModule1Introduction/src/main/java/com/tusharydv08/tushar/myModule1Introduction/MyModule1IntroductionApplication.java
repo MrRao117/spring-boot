ed32b8758a25f076189bbbf1f91925cbc1b66dc5
Tushar <tusharydv08@gmail.com>
2026-04-02 10:20:38 +0530

    Initial commit - Week 1 only

---
+++ Week1 Introduction to Spring and Spring Boot/myModule1Introduction/src/main/java/com/tusharydv08/tushar/myModule1Introduction/MyModule1IntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.tusharydv08.tushar.myModule1Introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyModule1IntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyModule1IntroductionApplication.class, args);
+	}
+
+}
---
+++ Week1 Introduction to Spring and Spring Boot/myModule1Introduction/src/test/java/com/tusharydv08/tushar/myModule1Introduction/MyModule1IntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tusharydv08.tushar.myModule1Introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyModule1IntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
