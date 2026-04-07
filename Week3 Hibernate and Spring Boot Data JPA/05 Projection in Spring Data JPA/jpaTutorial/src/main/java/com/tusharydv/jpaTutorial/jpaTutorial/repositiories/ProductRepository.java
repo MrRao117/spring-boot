0f94bd982a2e91e57fe96f716f042a4bfb6b9108
Tushar <tusharydv08@gmail.com>
2026-04-07 18:52:46 +0530

    Add Week 3: 05 Projection in Spring Data JPA

---
+++ Week3 Hibernate and Spring Boot Data JPA/05 Projection in Spring Data JPA/jpaTutorial/src/main/java/com/tusharydv/jpaTutorial/jpaTutorial/repositiories/ProductRepository.java
@@ -0,0 +1,31 @@
+package com.tusharydv.jpaTutorial.jpaTutorial.repositiories;
+
+import com.tusharydv.jpaTutorial.jpaTutorial.entities.ProductEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+    List<ProductEntity> findByTitle(String title, Pageable pageable);
+
+    List<ProductEntity> findByCreatedAtAfterOrderByTitle(LocalDateTime after);
+
+    List<ProductEntity> findByQuantityGreaterThanAndPriceLessThan(Integer quantity, BigDecimal price);
+
+
+    List<ProductEntity> findByTitleContainingIgnoreCase(String title, Pageable pageable);
+
+//    Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
+
+    @Query("select e from ProductEntity e where e.title=?1 and e.price=?2")
+    Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
+
+}
