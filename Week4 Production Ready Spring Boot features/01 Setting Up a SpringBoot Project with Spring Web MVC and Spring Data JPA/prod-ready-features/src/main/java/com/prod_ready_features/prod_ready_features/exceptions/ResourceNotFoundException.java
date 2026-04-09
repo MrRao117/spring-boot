dcfb61c122fcf28b3a4d7287c7dd4af0626ea43b
Tushar Yadav <tusharydv08@gmail.com>
2026-04-09 09:33:55 +0530

    Delete Week4 Production Ready Spring Boot features/01 Setting Up a SpringBoot Project with Spring Web MVC and Spring Data JPA/prod-ready-features directory

--- Week4 Production Ready Spring Boot features/01 Setting Up a SpringBoot Project with Spring Web MVC and Spring Data JPA/prod-ready-features/src/main/java/com/prod_ready_features/prod_ready_features/exceptions/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.prod_ready_features.prod_ready_features.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
